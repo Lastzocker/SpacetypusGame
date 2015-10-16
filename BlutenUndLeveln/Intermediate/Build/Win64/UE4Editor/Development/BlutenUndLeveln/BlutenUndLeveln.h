a05b42b4be959b038054257917dd41724ffd9cd5
FelixEmmerich <Felix-Emmerich@Freenet.de>
2015-10-16 22:04:26 +0200

    Excepted the Build/Win64/ folder from gitignore since for some dumb reason Unreal stores the base files for c++ there.
    Added the files included in that folder.

---
+++ BlutenUndLeveln/Intermediate/Build/Win64/UE4Editor/Development/BlutenUndLeveln/BlutenUndLeveln.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_BLUTENUNDLEVELN_H__
+#define __AUTO_BLUTENUNDLEVELN_H__
+//Last Write: 16.10.2015 16:55:57 +00:00
+#include "C:\Users\Felix\Desktop\Git\SpacetypusGame\BlutenUndLeveln\Source\BlutenUndLeveln\BlutenUndLeveln.h"
+#endif//__AUTO_BLUTENUNDLEVELN_H__
---
+++ BlutenUndLeveln/Intermediate/Build/Win64/UE4Editor/Development/BlutenUndLeveln/PCH.BlutenUndLeveln.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\Felix\Desktop\Git\SpacetypusGame\BlutenUndLeveln\Source\BlutenUndLeveln\BlutenUndLeveln.h"
