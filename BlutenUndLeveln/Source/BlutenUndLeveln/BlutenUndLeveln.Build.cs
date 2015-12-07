d72658f234259db97cef74482bc44dafcf17c99b
Felix Emmerich <Geckosandwich@live.de>
2015-12-07 13:45:11 +0100

    Integrated perlin noise library and added a Blueprint-callable perlin test called PerlinTest.

--- BlutenUndLeveln/Source/BlutenUndLeveln/BlutenUndLeveln.Build.cs
+++ BlutenUndLeveln/Source/BlutenUndLeveln/BlutenUndLeveln.Build.cs
@@ -5,44 +5,44 @@ using UnrealBuildTool;
 
 public class BlutenUndLeveln : ModuleRules
 {
-    //private string ModulePath
-    //{
-    //    get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
-    //}
+    private string ModulePath
+    {
+        get { return Path.GetDirectoryName( RulesCompiler.GetModuleFilename( this.GetType().Name ) ); }
+    }
 
-    //private string ThirdPartyPath
-    //{
-    //    get { return Path.GetFullPath(Path.Combine(ModulePath, "../../libnoise/")); }
-    //}
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
 
 	public BlutenUndLeveln(TargetInfo Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-	    //LoadLibNoise(Target);
+	    LoadLibNoise(Target);
 	}
 
-    //public bool LoadLibNoise(TargetInfo Target)
-    //{
-    //    bool isLibrarySupported = false;
+    public bool LoadLibNoise(TargetInfo Target)
+    {
+        bool isLibrarySupported = false;
 
-    //    if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
-    //    {
-    //        isLibrarySupported = true;
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
 
-    //        string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
-    //        string LibrariesPath = Path.Combine(ThirdPartyPath, "libnoise", "Libraries");
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "Libnoise", "Libraries");
 
-    //        PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libnoise." + ".lib"));
-    //    }
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libnoise." + PlatformString + ".lib"));
+        }
 
-    //    if (isLibrarySupported)
-    //    {
-    //        // Include path
-    //        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "libnoise", "Includes"));
-    //    }
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Libnoise", "Includes"));
+        }
 
-    //    Definitions.Add(string.Format("WITH_LIBNOISE_BINDING={0}", isLibrarySupported ? 1 : 0));
+        Definitions.Add(string.Format("WITH_LIBNOISE_BINDING={0}", isLibrarySupported ? 1 : 0));
 
-    //    return isLibrarySupported;
-    //}
+        return isLibrarySupported;
+    }
 }
